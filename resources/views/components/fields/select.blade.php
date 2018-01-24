a903817bf947b5c47c021c3c06f8b7fc8e9e02dc
Farid Inawan <frdteknikelektro@gmail.com>
2018-01-24 09:55:25 +0700

    Update some components

--- resources/views/components/fields/select.blade.php
+++ resources/views/components/fields/select.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group{{ $errors->has($field['name']) ? ' has-error' : '' }}">
   <label class="control-label">{{ !empty($field['label']) ? $field['label'] : title_case(str_replace('_', ' ', snake_case($field['name']))) }}{{ !empty($field['required']) ? '*' : '' }}</label>
-  <select class="form-control m-b" name="{{ $field['name'] }}">
+  <select class="form-control" name="{{ $field['name'] }}">
     @foreach ($field['options'] as $option)
-    <option value="{{ $option['value'] }}">{{ $option['text'] }}</option>
+    <option value="{{ $option['value'] }}" {{ $option['value'] == old($field['name'], isset($model) ? $model->{$field['name']} : null) ? 'selected' : '' }}>{{ $option['text'] }}</option>
     @endforeach
   </select>
   @if ($errors->has($field['name']))
