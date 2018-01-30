93fac8aea9063ef6fae8df12eff76734f1ea115f
Farid Inawan <frdteknikelektro@gmail.com>
2018-01-30 07:35:36 +0700

    Merge field components become one input component

--- resources/views/components/fields/checkbox.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="form-group{{ $errors->has($field['name']) ? ' has-error' : '' }}">
-  <label class="control-label">{{ !empty($field['label']) ? $field['label'] : title_case(str_replace('_', ' ', snake_case($field['name']))) }}{{ !empty($field['required']) ? '*' : '' }}</label>
-  @foreach ($field['checkboxes'] as $checkbox)
-    <div class="i-checks"><label> <input type="checkbox" value="{{ $checkbox['value'] }}"><i></i> {{ $checkbox['text'] }}</label></div>
-  @endforeach
-</div> 
